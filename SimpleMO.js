b854bb09a9bc53d952b575155e3ec74560b61235
Raja Sekhar <sapparaj262@gmail.com>
2017-04-04 01:46:23 +0530

    First program  in nodes js

---
+++ SimpleMO.js
@@ -0,0 +1,9 @@
+var operator = process.argv[2];
+var num1 = parseInt(process.argv[3]);
+var num2 = parseInt(process.argv[4]);
+if (operator == "+")
+console.log (num1+num2);
+else if (operator == "-")
+console.log (num1-num2);
+else if (operator == "*")
+console.log (num1*num2);
